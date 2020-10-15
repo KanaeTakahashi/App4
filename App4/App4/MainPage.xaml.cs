b92a39695d41dbd23f291b2a2526fcb3bfee230d
KanaeTakahashi <lak.takahashi@gmail.com>
2020-10-15 17:17:49 +0900

    プロジェクト ファイルを追加します。

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // イベントハンドラの紐づけ
+            NextButton.Clicked += NextButton_Clicked;
+        }
+
+        private void NextButton_Clicked(object sender, EventArgs e)
+        {
+            var current = int.Parse(lblCount.Text);
+            lblCount.Text = (current + 1).ToString();
+        }
+
+        public void Button_OnClicked(object sender, ClickedEventArgs e)
+        {
+            var current = int.Parse(lblCount.Text);
+            lblCount.Text = (current + 1).ToString();
+        }
+    }
+}
