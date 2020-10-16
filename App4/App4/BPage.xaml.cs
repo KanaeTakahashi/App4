861d3338f163125c01916c12cc8aa070611aaa34
KanaeTakahashi <lak.takahashi@gmail.com>
2020-10-16 15:22:10 +0900

    マップにピンを立てる、取得した位置情報のピンを立てる

--- App4/App4/BPage.xaml.cs
+++ App4/App4/BPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.GoogleMaps;
+using Xamarin.Forms.Markup;
 using Xamarin.Forms.Xaml;
 
 namespace App4
@@ -32,6 +33,42 @@ namespace App4
 
             var result = await GetCurrentPosition();
             editPosition.Text = ProcessingForDisplay(result);
+            AddPosition(result);
+        }
+
+        /// <summary>
+        /// 位置の追加
+        /// </summary>
+        /// <param name="p"></param>
+        private void AddPosition(Plugin.Geolocator.Abstractions.Position p)
+        {
+            var pin = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "現在位置",
+                Address = "",
+                Position = new Xamarin.Forms.GoogleMaps.Position(p.Latitude, p.Longitude),
+                //Rotation = 33.3f,
+                Tag = "",
+                IsDraggable = true,
+            };
+            MyMap.Pins.Add(pin);
+        }
+
+        // MAPの長押しイベント
+        private void MyMap_MapLongClicked(object sender, MapLongClickedEventArgs e)
+        {
+            var pin = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Long Tapped",
+                Address = "Added New Pin",
+                Position = e.Point,//長押しした地図上の位置
+                Rotation = 33.3f,
+                Tag = "",
+                IsDraggable = true,
+            };
+            MyMap.Pins.Add(pin);
         }
 
         /// <summary>
@@ -62,7 +99,7 @@ namespace App4
 
                 if (position != null)
                 {
-                    //got a cahched position, so let's use it.
+                    //got a cahched p, so let's use it.
                     return position;
                 }
 
@@ -109,8 +146,34 @@ namespace App4
 
             try
             {
-                //東京へ移動させる
-                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Xamarin.Forms.GoogleMaps.Position(35.6762, 139.6503), Distance.FromKilometers(100)));
+                //高知駅へ移動させる
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Xamarin.Forms.GoogleMaps.Position(33.5684, 133.5566), Distance.FromKilometers(100)));
+
+                //ピンを立てる
+                var pin = new Pin()
+                {
+                    Type = PinType.Place,
+                    Label = "ピンを追加テスト",
+                    Address = "Hello Tokyo",
+                    Position = new Xamarin.Forms.GoogleMaps.Position(35.6762, 139.6503),  //東京
+                    Rotation = 0,  //  -33.3f などとするとピンを傾けることができる
+                    Tag = "",
+                    IsDraggable = true,//これだけでピンを長押しすると移動モードになる
+                };
+                //マップへ追加
+                MyMap.Pins.Add(pin);
+
+                //var pin2 = new Pin()
+                //{
+                //    Type = PinType.Place,
+                //    Label = "高知駅",
+                //    Address = "Hello Kochi",
+                //    Position = new Xamarin.Forms.GoogleMaps.Position(33.5658, 133.5437),  //高知駅
+                //    Rotation = 0,
+                //    Tag = "",
+                //    IsDraggable = true,
+                //};
+                //MyMap.Pins.Add(pin2);
             }
             catch (Exception ex)
             {
